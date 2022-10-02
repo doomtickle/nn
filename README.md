0e1d078041e916950d00e36c5fb96122eee3fcbb
Daron Adkins <daron.adkins@gmail.com>
2022-10-02 17:15:50 -0400

    basic perceptron

--- README.md
+++ README.md
@@ -26,3 +26,16 @@ Perceptron Algorithm
 1. For every input multiply that input by its weight.
 2. Sum all of the weighted inputs.
 3. Compute the output of the perceptron based on that sum passed through an activation function
+
+Supervised Learning Algorithm
+
+1. Provide the perceptron with inputs for which there is a known answer.
+2. Ask the perceptron to guess an answer.
+3. Comput the error. (Did it get the answer right or wrong?)
+4. Adjust all the weights according to the error.
+5. GOTO 1
+
+∆W = `Error / Input`
+New Weight = `Weight + Error * Input * Learning Rate`
+
+Calculate using "Gradient Descent"
--- main.go
+++ main.go
@@ -6,6 +6,10 @@ import (
 	"time"
 )
 
+const (
+	lr = 0.1
+)
+
 type Perceptron struct {
 	Weights []float64
 }
@@ -38,6 +42,17 @@ func (p *Perceptron) Guess(inputs []float64) int {
 	return output
 }
 
+func (p *Perceptron) Train(inputs []float64, target int) {
+  var ws []float64
+	guess := p.Guess(inputs)
+	err := target - guess
+	for i, w := range p.Weights {
+		ws = append(ws, (w + float64(err) * inputs[i] * lr))
+	}
+
+  p.Weights = ws
+}
+
 // activation function
 func sign(n float64) int {
 	if n >= 0 {
@@ -47,15 +62,50 @@ func sign(n float64) int {
 	return -1
 }
 
-var (
-	inputs = []float64{-1, 0.5}
-)
+type TrainingPoint struct {
+	X, Y  float64
+	Label int
+}
+
+func trainingData(n int) []TrainingPoint {
+	var (
+		p  TrainingPoint
+		td []TrainingPoint
+	)
+
+	for i := 0; i < n; i++ {
+		rp := randFloats(0, 400, 2)
+		p.X = rp[0]
+		p.Y = rp[1]
+		if p.X > p.Y {
+			p.Label = 1
+		} else {
+			p.Label = -1
+		}
+
+		td = append(td, p)
+	}
+
+	return td
+}
 
 func main() {
-  rand.Seed(time.Now().UnixNano())
-
-	for i := 0; i < 150; i++ {
-    p := NewPerceptron(len(inputs))
-		fmt.Println(p.Guess(inputs))
+	rand.Seed(time.Now().UnixNano())
+	p := NewPerceptron(2)
+	data := trainingData(100)
+  correct := 0
+	for i := 0; i < 10; i++ {
+    correct = 0
+		for _, d := range data {
+			inputs := []float64{d.X, d.Y}
+			target := d.Label
+			p.Train(inputs, target)
+      guess := p.Guess(inputs)
+      fmt.Printf("%d, %d\n", guess, target)
+			if guess == target {
+				correct = correct + 1
+			}
+		}
+		fmt.Printf("Correct: %d/100\n", correct)
 	}
 }
